aec10021d4ce07ef6b1abf385af9121ec2c96c95
Andriy <andrijcopek696@gmail.com>
2023-06-12 11:15:14 +0300

    Entities have been added to DB and seeded info

---
+++ ShopOnline.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopOnline.Api.Entities;
+
+namespace ShopOnline.Api.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Cart>().HasData(
+                new Cart { Id = 1, UserId = 1 },
+                new Cart { Id = 2, UserId = 2 },
+                new Cart { Id = 3, UserId = 3}
+            );
+            modelBuilder.Entity<CartItem>().HasData(
+                new CartItem { Id = 1, CartId = 1, ProductId = 1, Qty = 10 },
+                new CartItem { Id = 2, CartId = 2, ProductId = 2, Qty = 20 },
+                new CartItem { Id = 3, CartId = 3, ProductId = 3, Qty = 30 }
+                );
+            modelBuilder.Entity<Product>().HasData(
+                new Product 
+                { 
+                    Id = 1,
+                    Name = "Watermelon",
+                    Description = "Sweety watermelon",
+                    CategoryId = 1,
+                    ImageURL = "",
+                    Price = 2,
+                    Qty = 6
+                },
+                new Product
+                {
+                    Id = 2,
+                    Name = "Banana",
+                    Description = "Yellow african bananas",
+                    CategoryId = 1,
+                    ImageURL = "",
+                    Price = 5,
+                    Qty = 16
+                },
+                new Product
+                {
+                    Id = 3,
+                    Name = "Orange",
+                    Description = "Sweet and taste orange",
+                    CategoryId = 1,
+                    ImageURL = "",
+                    Price = 4,
+                    Qty = 10
+                }
+                );
+            modelBuilder.Entity<ProductCategory>().HasData(
+                new ProductCategory { Id = 1, Name = "Fruits" },
+                new ProductCategory { Id = 2, Name = "Furnitures" },
+                new ProductCategory { Id = 3, Name = "For graden" }
+                );
+        }
+    }
+}
---
+++ ShopOnline.Api/Migrations/20230612075124_AddTablesToDataBase.cs
@@ -0,0 +1,142 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShopOnline.Api.Migrations
+{
+    public partial class AddTablesToDataBase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CartItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CartId = table.Column<int>(type: "int", nullable: false),
+                    ProductId = table.Column<int>(type: "int", nullable: false),
+                    Qty = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CartItems", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Carts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Carts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProductCategories",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProductCategories", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Products",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ImageURL = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Qty = table.Column<int>(type: "int", nullable: false),
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Products", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "CartItems",
+                columns: new[] { "Id", "CartId", "ProductId", "Qty" },
+                values: new object[,]
+                {
+                    { 1, 1, 1, 10 },
+                    { 2, 2, 2, 20 },
+                    { 3, 3, 3, 30 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Carts",
+                columns: new[] { "Id", "UserId" },
+                values: new object[,]
+                {
+                    { 1, 1 },
+                    { 2, 2 },
+                    { 3, 3 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ProductCategories",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Fruits" },
+                    { 2, "Furnitures" },
+                    { 3, "For graden" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "CategoryId", "Description", "ImageURL", "Name", "Price", "Qty" },
+                values: new object[,]
+                {
+                    { 1, 1, "Sweety watermelon", "", "Watermelon", 2m, 6 },
+                    { 2, 1, "Yellow african bananas", "", "Banana", 5m, 16 },
+                    { 3, 1, "Sweet and taste orange", "", "Orange", 4m, 10 }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CartItems");
+
+            migrationBuilder.DropTable(
+                name: "Carts");
+
+            migrationBuilder.DropTable(
+                name: "ProductCategories");
+
+            migrationBuilder.DropTable(
+                name: "Products");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
